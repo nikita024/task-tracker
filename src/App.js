d857260494b698a9bbb42f77081d6721b4989e3b
nikita.roy <nikitaroy024@gmail.com>
2024-10-29 20:31:15 +0530

    change in url

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
